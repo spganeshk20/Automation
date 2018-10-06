028264b6e392174b069c36d2aa554990e3dbd420
spganeshk20 <spganeshk2094@gmail.com>
2018-10-06 19:37:40 +0530

    Automation click script

---
+++ Python-Automation-Scripts/click.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python2.7
+
+"""
+------------------------------------------
+		License Information
+------------------------------------------
+        GNU GENERAL PUBLIC LICENSE
+        Version 3, 29 June 2007
+Copyright (C) 2007 Free Software Foundation, Inc. 
+https://fsf.org/ Everyone is permitted to copy and 
+distribute verbatim copies of this license document, 
+but changing it is not allowed.
+
+Note: For detailed license information, 
+please read the LICENSE file in the repository[Repository Name: Automation]
+"""
+
+"""
+script name   : click.py
+Functionality : Keeps the window active by clicking every 10 seconds
+Created on    : 21 SEP 2017
+"""
+
+__author__ = 'Ganesh'
+
+import win32api, win32con
+import time
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+
+
+while 1:
+    click(10,10)
+    time.sleep(10)
--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-# Automation
+# Author : Ganesh
 
-Author = Ganesh
+# License Information
+                       GNU GENERAL PUBLIC LICENSE
+                       Version 3, 29 June 2007
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+ 
+**Note: For detailed license information, please read the LICENSE file in the repository[Repository Name: Projects-IoT-Python-Embedded-C]**
+
+# Projects-IoT-Python-Embedded-C
+
+The Repository contains IoT End-to-End(E2E) solution projects developed using the below following programming languages and concepts, Hardware platform and cloud platforms
+
+Programming languages
+  1. Embedded C
+  2. Python
+
+Hardware platform 
+  1. Arduino UNO
+  2. Arduino MEGA
+  3. Raspberry PI
+  4. Intel edison
+
+cloud platforms
+  1. Thingspeak
+  2. IBM Bluemix services
